2702d8db2c0f61d67fedbb2e0fd67c5d239f6ad8
THUNDERBOLT\ThunderBolt <vivekkadyan1997@gmail.com>
2021-03-08 16:58:16 +0530

    models

---
+++ mHealthSystem/src/main/java/com/cg/mHealthSystem/models/Department.java
@@ -0,0 +1,35 @@
+package com.cg.mHealthSystem.models;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Department {
+	
+	private String deptName;
+	private String phoneNo;
+	private String emailId;
+	
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+	
+	
+	
+
+}
---
+++ mHealthSystem/src/main/java/com/cg/mHealthSystem/models/Nurse.java
@@ -0,0 +1,39 @@
+package com.cg.mHealthSystem.models;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Nurse {
+	private Integer nurseId;
+	private Integer deptId;
+	private Integer userId;
+	private Integer empId;
+	
+	public Integer getNurseId() {
+		return nurseId;
+	}
+	public void setNurseId(Integer nurseId) {
+		this.nurseId = nurseId;
+	}
+	public Integer getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(Integer deptId) {
+		this.deptId = deptId;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public Integer getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	
+	
+
+}
