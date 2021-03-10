f3cdfa09496d49efdcad83384609683c1f52f6aa
THUNDERBOLT\ThunderBolt <vivekkadyan1997@gmail.com>
2021-03-10 11:32:03 +0530

    controller,service with implementation for doctor, append one method.

---
+++ mHealthSystem/src/main/java/com/cg/mHealthSystem/Controllers/DoctorController.java
@@ -0,0 +1,47 @@
+package com.cg.mHealthSystem.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.mHealthSystem.entity.Appointments;
+import com.cg.mHealthSystem.entity.Doctor;
+import com.cg.mHealthSystem.entity.PatientDetails;
+import com.cg.mHealthSystem.entity.PatientRecords;
+import com.cg.mHealthSystem.services.DoctorService;
+
+
+
+@RestController
+@RequestMapping("/doctor")
+public class DoctorController {
+	@Autowired
+	private DoctorService doctorService ;
+	
+	@PostMapping("/addDoctor")
+	public Doctor addDoctor(@RequestBody Doctor doctor )
+	{
+		return doctorService.addDoctor(doctor);
+	}
+	 @PutMapping("/updateProfile/{deptId:.+}/patientDetails/{doctorId}")
+	 public Doctor updateProfile(@PathVariable Integer doctorId, @PathVariable Integer deptId)
+	 {
+	        return doctorService.updateProfile(doctorId, deptId);
+	 }
+	 @GetMapping("/viewAppointment/{appointmentId}")
+	    public Appointments viewAppointment(@PathVariable Integer appointmentId)
+	    {
+	        return doctorService.viewAppointment(appointmentId);
+	    }
+	 @GetMapping("/patientRecord/{patientId}")
+	    public PatientRecords patientRecord(@PathVariable Integer patientId)
+	    {
+	        return doctorService.patientRecord(patientId);
+	    }
+
+}
---
+++ mHealthSystem/src/main/java/com/cg/mHealthSystem/services/DoctorService.java
@@ -0,0 +1,16 @@
+package com.cg.mHealthSystem.services;
+
+import com.cg.mHealthSystem.entity.Appointments;
+import com.cg.mHealthSystem.entity.Doctor;
+import com.cg.mHealthSystem.entity.PatientRecords;
+
+
+public interface DoctorService {
+	
+	
+	Doctor updateProfile(Integer doctorId, Integer deptId);
+	Appointments viewAppointment(Integer appointmentId);
+	PatientRecords patientRecord(Integer patientId);
+	Doctor addDoctor(Doctor doctor);
+
+}
